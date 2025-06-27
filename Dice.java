b6dc3cb1a1fe91131fcc72d595040c5cf8888a0a
小野寺祐太 <onoderayuuta@wlan-napt-008.u-aizu.ac.jp>
2025-06-27 12:26:02 +0900

    first commit

---
+++ Dice.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class Dice {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int sum = 0;
+        int di = rand.nextInt(6) + 1;
+        sum += di;
+        System.out.println("Dic 1: " + di);
+        di = rand.nextInt(6) + 1;
+        sum += di;
+        System.out.println("Dic 2: " + di);
+        System.out.println("Total value: " + sum);
+        
+    }
+}
