889f20dcdcb201330b9dc1906abaafc94277e290
小野寺祐太 <onoderayuuta@wlan-napt-003.u-aizu.ac.jp>
2025-07-03 16:16:36 +0900

    Won/lost massage added

--- Dice.java
+++ Dice.java
@@ -10,6 +10,11 @@ public class Dice {
         sum += di;
         System.out.println("Dic 2: " + di);
         System.out.println("Total value: " + sum);
+        if(sum > 7) {
+            System.out.println("You won");
+        } else {
+            System.out.println("You lose");
+        }
         
     }
 }
